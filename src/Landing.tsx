10d4cfa17f576ac10d185a125353c6e16989b3a8
ayyashcihuy <ayyashcihuy@gmail.com>
2024-01-02 20:48:03 +0700

    init project

---
+++ src/Landing.tsx
@@ -0,0 +1,11 @@
+export default function LandingPage() {
+    return (
+        <div className="h-screen w-full bg-[#1E1E1E] flex flex-col justify-between">
+            <nav className="flex flex-row justify-between text-white">
+                <div>
+                    <a href="home">Home</a>
+                </div>
+            </nav>
+        </div>
+    )
+}
